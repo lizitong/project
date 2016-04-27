24775ebc83b56212b437e252e74ab555396caa1f
lizitong <lizitong99@gmail.com>
2016-04-28 03:12:59 +0800

    Update test.py
    This version is correct with time+location format for drop-offs, but point_inside_polygon method seems to be incorrect, while shapely is extremely slow.

--- src/test.py
+++ src/test.py
@@ -6,14 +6,35 @@ tip_amount			tolls_amount			improvement_surcharge	total_amount
 """
 from pyspark import SparkConf, SparkContext, SparkFiles
 from operator import add
-from shapely.geometry import MultiPoint, Point
+#from shapely.geometry import MultiPoint, Point
 import shapefile
 
-APP_NAME = "First_Attempt"
+APP_NAME = "Traffic_Peak_Analysis"
+
+# determine if a point is inside a given polygon or not
+# Polygon is a list of (x,y) pairs.
+def point_inside_polygon(x,y,poly):
+    n = len(poly)
+    inside =False
+
+    p1x,p1y = poly[0]
+    for i in range(n+1):
+        p2x,p2y = poly[i % n]
+        if y > min(p1y,p2y):
+            if y <= max(p1y,p2y):
+                if x <= max(p1x,p2x):
+                    if p1y != p2y:
+                        xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
+                    if p1x == p2x or x <= xinters:
+                        inside = not inside
+        p1x,p1y = p2x,p2y
+
+    return inside
+
 #15/1/2015 19:23
 #Only use hour and minute, converte minute to intervals of 10.
 def parse(line):
-	line = line.split(",")
+	#line = line.split(",")
 	datetime = line[2].split()
 	try:
 		#hour = int(time[0])
@@ -30,32 +51,60 @@ def parse(line):
   		return ("Invalid Point", 1)
 	#print "**************************"
 	#print "newLine is:", newLine
-	point = Point(x, y)
+	#point = Point(x, y)
 	county = "Not found"
 	srs =shapeRecs.value
 	for sr in srs:
 		coords = sr.shape.points
+		"""
 		polygon = MultiPoint(coords).convex_hull
 		if point.within(polygon):
 			county = sr.record[6]
-	newLine = (time + "," + county, 1)
+		"""
+		if point_inside_polygon(x,y,coords):
+			county = sr.record[6]
 	if county == "Not found":
-		print "********************"
-		print "County not found, point is:", x, y
+		county = "Richmond"
+		#print "********************"
+		#print "County not found, point is:", x, y
+	newLine = (time + "," + county, 1)
 	return newLine
 
+def deleteInvalidLines(line):
+	try:
+		int(line[0])
+	except:
+		return False
+	return line[9] != "0.0" and line[10] != "0.0"
+
 def main(sc):
-	trips = sc.textFile("test.csv").map(parse).reduceByKey(add).sortBy(lambda x:x[1], False)
-	trips.saveAsTextFile("test_output")
+	"""
+	trips = sc.textFile("/taxidata/*/")
+	mappedTrips = trips.map(lambda line:line.split(",")).filter(deleteInvalidLines)
+	# print mappedTrips.collect()
+	newTrips = mappedTrips.map(parse)
+	#print newTrips.collect()
+	testTrips = mappedTrips.reduceByKey(add)
+	print testTrips.collect()
+	sortedTrips = testTrips.sortBy(lambda x:x[1], False)
+	sortedTrips.saveAsTextFile("test_output")
+	"""
+	trips = sc.textFile("/taxidata/*/").map(lambda line:line.split(",")).filter(deleteInvalidLines)
+	sortedTrips = trips.map(parse).reduceByKey(add).sortBy(lambda x:x[1], False)
+	sortedTrips.saveAsTextFile("test_output")
 
 if __name__ == "__main__":
-	conf = SparkConf().setAppName(APP_NAME)
+	conf = SparkConf()
+	conf.setAppName(APP_NAME)
+	conf.setMaster('yarn-client')
+	#conf.setMaster('local[*]')
 	sc = SparkContext(conf=conf)
 	sc.addPyFile("shapefile.py")
-	sc.addFile("../NY_counties_clip/NY_counties_clip.dbf")
-	sc.addFile("../NY_counties_clip/NY_counties_clip.shx")
-	sc.addFile("../NY_counties_clip/NY_counties_clip.shp")
-	shapefilePath = SparkFiles.get("NY_counties_clip.shp")
+	#sc.addFile("../NY_counties_clip/NY_counties_clip.dbf")
+	#sc.addFile("../NY_counties_clip/NY_counties_clip.shx")
+	#sc.addFile("../NY_counties_clip/NY_counties_clip.shp")
+	#shapefilePath = SparkFiles.get("NY_counties_clip.shp")
+	shapefilePath = "../NY_counties_clip/NY_counties_clip"
 	sf = shapefile.Reader(shapefilePath)
 	shapeRecords = sf.shapeRecords()
 	shapeRecs = sc.broadcast(shapeRecords)
